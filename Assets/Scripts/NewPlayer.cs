38153afd1452ce495a138d904e5f199f1c7ba52b
KABOOTAR-1 <bt20cse052@iiitn.ac.in>
2022-10-09 23:19:08 +0530

    Changes

--- Assets/Scripts/NewPlayer.cs
+++ Assets/Scripts/NewPlayer.cs
@@ -19,7 +19,7 @@ public class NewPlayer : MonoBehaviour
     }
     void Des()
     {
-
+        if(playerclone!=null)
             playerclone.SetActive(false);
             Invoke("Des", 5);
             
